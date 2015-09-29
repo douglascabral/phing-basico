1f9d711d4ed45313f7f8c5e5aa1e53ceb45480fc
Douglas Cabral <douglas.cabral@fiap.com.br>
2015-09-29 14:08:24 -0300

    Forçado erro no teste unitário

--- tests/ClasseTest.php
+++ tests/ClasseTest.php
@@ -4,6 +4,6 @@ class ClasseTest extends PHPUnit_Framework_TestCase
 	public function testIsBool() {
 		$classe = new Classe;
 		$this->assertTrue($classe->isBool(true));
-		//$this->assertTrue(false);
+		$this->assertTrue(false);
 	}
 }
