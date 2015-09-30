3e0f940109c4846c51e25461e4ad85cd8f5f136c
Douglas Cabral <douglas.cabral@fiap.com.br>
2015-09-30 18:23:58 -0300

    Configurado basico para CI com PHP

--- src/Classe.php
+++ src/Classe.php
@@ -1,8 +1,29 @@
-<?php
-class Classe {
-	
-	public function isBool($bool) {
-		return $bool;
-	}
-	
+<?php 
+/**
+* Documentação do arquivo
+* @category Estudos
+* @package Src
+* @author Douglas Cabral <contato@douglascabral.com.br>
+* @license https://opensource.org/licenses/MIT MIT
+*/
+
+define("CONSTANTE", "0");
+
+/**
+* Descrição da classe
+*/
+class Classe
+{
+    
+    
+    /**
+     * Descrição do método isBool
+     * @param  boolean $bool Parametro para verificação
+     * @return boolean Retorna verdadeiro se for booleano
+     */
+    public function isBool($bool) 
+    {
+        return $bool;
+    }
+
 }
--- tests/ClasseTest.php
+++ tests/ClasseTest.php
@@ -4,6 +4,5 @@ class ClasseTest extends PHPUnit_Framework_TestCase
 	public function testIsBool() {
 		$classe = new Classe;
 		$this->assertTrue($classe->isBool(true));
-		$this->assertTrue(false);
 	}
 }
